d728f7b3efd63817de973f1180ad4137aa88c6a6
denys.vovk <denys.vovk@worldapp.com>
2019-12-24 13:55:47 +0200

    index view

--- src/main/java/com/devo/root/index/IndexController.java
+++ src/main/java/com/devo/root/index/IndexController.java
@@ -1,7 +1,15 @@
 package com.devo.root.index;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
 
 }
