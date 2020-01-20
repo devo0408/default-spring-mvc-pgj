a25075729ec4e78cfefe756ce0c629a0175aeae9
devo0408 <vovk.dev@gmail.com>
2020-01-20 22:04:08 +0200

    H2 features

--- src/test/java/com/example/demo/GlobalServerValueServiceTest.java
+++ src/test/java/com/example/demo/GlobalServerValueServiceTest.java
@@ -1,26 +1,11 @@
 package com.example.demo;
 
-import com.devo.data.GlobalServerValueRepository;
-import com.devo.data.GlobalServerValueService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class GlobalServerValueServiceTest {
 
-    //
-    @Autowired // let Spring instantiate the instance to test
-    private GlobalServerValueService globalServerValueService;
-
-    @MockBean // let Spring instantiate the instance to test
-    private GlobalServerValueRepository globalServerValueRepository;
-
-    @Test
-    public void performProvision_shouldPass() {
-        UserData userData = new UserData("userid", 30, 1, "spot", 1);
-
-        String result = provisionService.performProvision(userData);
-        assertThat(result, is(equalTo("Success: User userid has been added to the database")));
-    }
+    // TODO: 20.01.2020 implement test for Service  
+    
 }
